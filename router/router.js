f1a87dcaadcafa92d80fcd4f2571d87d5b6a1989
njbullis <55627683+njbullis@users.noreply.github.com>
2019-12-21 10:38:05 -0500

    Add files via upload
    fixed router file and other revisions

--- router/router.js
+++ router/router.js
@@ -5,7 +5,6 @@ const express = require('express');
 const router = express.Router();
 console.log('asdf');
 
-
 router.post('/submissions', function (request, response, next) {
   response.send('Create Submission');
   next();
@@ -21,21 +20,9 @@ router.post('/login', function (request, response, next) {
   next();
 });
 
-
-router.get('/entries', function (request, response, next) {
-  response.render('Submissions');
+router.get('/entries', function (_request, response, next) {
+  response.send('Submissions');
   next();
 });
-
-async function read() {
-  let json = await readFile(dbPath);
-  return JSON.parse(json);
-}
-
-async function write(dbItems) {
-  // The parameters for `null` and `2` are so it's formatted with 2 spaces of indentation
-  let json = JSON.stringify(dbItems, null, 2);
-  await writeFile(dbPath, json);
-}
 
 module.exports = router;
